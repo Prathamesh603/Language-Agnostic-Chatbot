2a31bc594dc858dc5c0819e22e539c11bd81d050
Pratham Rajput <prathamrajput709@gmail.com>
2026-01-02 10:49:58 +0530

    from langchain_text_splitters import RecursiveCharacterTextSplitter changed the new way to import the textsplitter

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from groq import Groq 
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
